a00396dda8cffe4c7fb9146afb00be9cc4443c92
Igor Bonadio <igorbonadio@gmail.com>
2014-08-18 15:34:45 -0300

    Adds Makefile and first binary

---
+++ Config.mk
@@ -0,0 +1,42 @@
+
+############################################################
+##     UNCOMMENT ANY TARGET TO OVERWRITE THE DEFAULT!     ##
+############################################################
+
+# Project setting
+# PROJECT  := # Project name. Default is 'Default'
+# VERSION  := # Version. Default is '1.0'
+
+# Program settings
+BIN := Hello
+# BIN      := # Binaries' names. If a subdir of the source
+#             # directories has the same name of this bin,
+#             # this dir and all subdir will be compiled
+#             # only for this specific binary.
+# SBIN     := # Same as above, but for shell-only binaries.
+# LIBEXEC  := # Again, but for binaries runnable only by
+#             # other programs, not normal users.
+
+# IGNORED  := # Files within Make dirs to be ignored.
+
+# ARLIB    := # Static/Shared libraries' names. If one is a
+# SHRLIB   := # lib, all source files will make the library.
+
+# Flags
+# ASFLAGS  := # Assembly Flags
+# CFLAGS   := # C Flags
+# CXXFLAGS := # C++ Flags
+# LDFLAGS  := # Linker flags
+
+# Documentation
+# LICENSE  := # File with a License (def: LICENSE)
+# NOTICE   := # File with a Notice of the License, to be used
+#             # in the beggining of any file (def: NOTICE).
+# DOXYFILE := # Config file for Doxygen (def: Doxyfile)
+
+# Package info
+# MAINTEINER_NAME := # Your name
+# MAINTEINER_MAIL := # your_name@mail.com
+# SYNOPSIS        := # One-line description of the program
+# DESCRIPTION     := # Longer description of the program
+
---
+++ src/Hello.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+    printf("Hello World!\n");
+    return 0;
+}
