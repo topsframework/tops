b8bb7909cec180e9c12b49d553826eb8ff535064
Renato Cordeiro Ferreira <renato.cferreira@hotmail.com>
2014-08-20 19:54:08 -0300

    Defines basic configurations

--- Config.mk
+++ Config.mk
@@ -1,42 +1,28 @@
 
 ############################################################
-##     UNCOMMENT ANY TARGET TO OVERWRITE THE DEFAULT!     ##
+##               ToPS Makefile Configuration              ##
 ############################################################
 
 # Project setting
-# PROJECT  := # Project name. Default is 'Default'
-# VERSION  := # Version. Default is '1.0'
+PROJECT  := ToPS
+VERSION  := 2.0.0
 
 # Program settings
 BIN := Hello
-# BIN      := # Binaries' names. If a subdir of the source
-#             # directories has the same name of this bin,
-#             # this dir and all subdir will be compiled
-#             # only for this specific binary.
-# SBIN     := # Same as above, but for shell-only binaries.
-# LIBEXEC  := # Again, but for binaries runnable only by
-#             # other programs, not normal users.
 
-# IGNORED  := # Files within Make dirs to be ignored.
-
-# ARLIB    := # Static/Shared libraries' names. If one is a
 # SHRLIB   := # lib, all source files will make the library.
 
 # Flags
-# ASFLAGS  := # Assembly Flags
-# CFLAGS   := # C Flags
 # CXXFLAGS := # C++ Flags
 # LDFLAGS  := # Linker flags
 
-# Documentation
-# LICENSE  := # File with a License (def: LICENSE)
-# NOTICE   := # File with a Notice of the License, to be used
-#             # in the beggining of any file (def: NOTICE).
-# DOXYFILE := # Config file for Doxygen (def: Doxyfile)
+GIT_DEPENDENCY := \
+    googletest => http://git.chromium.org/external/googletest.git
 
 # Package info
-# MAINTEINER_NAME := # Your name
-# MAINTEINER_MAIL := # your_name@mail.com
-# SYNOPSIS        := # One-line description of the program
-# DESCRIPTION     := # Longer description of the program
-
+MAINTEINER_NAME := Ígor Bonadio e Renato Cordeiro
+MAINTEINER_MAIL := igorbonadio@gmail.com, renatocordeiroferreira@gmail.com
+SYNOPSIS        := Toolkit of Probabilistic Sequences (ToPS)
+DESCRIPTION     := ToPS is an objected-oriented framework that \
+                   facilitates the integration of probabilistic \
+                   models for sequences over a user defined alphabet.
