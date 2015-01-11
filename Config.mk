186f3e050de298f0eeb38abb32f78de4e8b18e84
Renato Cordeiro Ferreira <renato.cferreira@hotmail.com>
2015-01-11 20:41:16 -0200

    Updates Config.mk to new version

--- Config.mk
+++ Config.mk
@@ -1,21 +1,21 @@
 
 ########################################################################
-##                     ToPS Makefile Configuration                    ##
+##                    ToPS Makefile Configuration                     ##
 ########################################################################
 
 # Project settings
 # ==================
 PROJECT         := ToPS
 VERSION         := 2.0.0
-GIT_REMOTE      := git@bitbucket.org:igorbonadio/topspp.git
+GIT_REMOTE_PATH := git@bitbucket.org:igorbonadio/topspp.git
 
 # Project info
-# ===============
+# ==============
 COPYRIGHT       := ToPS
-MAINTEINER_NAME := Renato Cordeiro Ferreira,\
-                   Ígor Bonadio
-MAINTEINER_MAIL := renatocordeiroferreira@gmail.com,\
-                   igorbonadio@gmail.com
+MAINTEINER_NAME := Ígor Bonadio,\
+                   Renato Cordeiro Ferreira
+MAINTEINER_MAIL := igorbonadio@gmail.com,\
+                   renatocordeiroferreira@gmail.com
 SYNOPSIS        := Toolkit of Probabilistic Sequences (ToPS)
 DESCRIPTION     := ToPS is an objected-oriented framework that  \
                    facilitates the integration of probabilistic \
@@ -23,19 +23,31 @@ DESCRIPTION     := ToPS is an objected-oriented framework that  \
 
 # Program settings
 # ==================
-BIN      := Hello
-TESTBIN  := test
+BIN             := Hello
+TESTBIN         := test
 
 # Dependencies
 # ==============
-GIT_DEPENDENCY := \
-    gmock       => http://git.chromium.org/external/googlemock.git\
-                   cd make && make gmock.a && mv gmock.a libgmock.a,\
-    gmock/gtest => http://git.chromium.org/external/googletest.git\
-                   cd make && make gtest.a && mv gtest.a libgtest.a
+GIT_DEPENDENCY  := # List of git dependencies in the form: 
+                   # DEP_NAME => dep_path build_cmd        
+WEB_DEPENDENCY  := # Same as above, but for URL downloads  
+                   # with 'curl -o' (default) or 'wget -O' 
+
+# Paths
+# =======
+ASLIBS          := # Assembly paths
+CLIBS           := # C paths
+CXXLIBS         := # C++ paths
+LDLIBS          := # Linker paths
 
 # Flags
 # =======
-CXXFLAGS += -ansi -Wall -pedantic -O2 -std=c++11 -pthread
-LDFLAGS  := -pthread -lgmock -lgtest
-LDLIBS   := -L external/gmock/gtest/make/ -L external/gmock/make/ 
+CPPFLAGS        := # Precompiler Flags
+ASFLAGS         := # Assembly Flags
+CFLAGS          := # C Flags
+CXXFLAGS        := -ansi -Wall -pedantic -O2 -std=c++11
+LDFLAGS         := # Linker flags
+
+# Makeball list
+# ===============
+include conf/googletest.mk
