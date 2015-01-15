bf19cc7dc2853eb785a75ed45500e9c499860995
Igor Bonadio <igorbonadio@gmail.com>
2015-01-15 14:45:00 -0200

    Adds Sequence

---
+++ include/model/Sequence.hpp
@@ -0,0 +1,14 @@
+#ifndef SEQUENCE_HPP_
+#define SEQUENCE_HPP_
+
+#include <vector>
+
+#include "Symbol.hpp"
+
+namespace tops {
+    namespace model {
+        typedef std::vector<Symbol> Sequence;
+    }
+}
+
+#endif
