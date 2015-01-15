a2e888de97b94393dd47e39f6fc73e78f61f5a28
Igor Bonadio <igorbonadio@gmail.com>
2015-01-15 11:36:11 -0200

    Adds GeneralizedHiddenMarkovModel class

---
+++ include/model/GeneralizedHiddenMarkovModel.hpp
@@ -0,0 +1,13 @@
+#ifndef GENERALIZED_HIDDEN_MARKOV_MODEL_HPP_
+#define GENERALIZED_HIDDEN_MARKOV_MODEL_HPP_
+
+#include "DecodableModel.hpp"
+
+namespace tops {
+    namespace model {
+        class GeneralizedHiddenMarkovModel : DecodableModel {
+        };
+    }
+}
+
+#endif
---
+++ src/model/GeneralizedHiddenMarkovModel.cpp
@@ -0,0 +1,7 @@
+#include "GeneralizedHiddenMarkovModel.hpp"
+
+namespace tops {
+    namespace model {
+
+    }
+}
