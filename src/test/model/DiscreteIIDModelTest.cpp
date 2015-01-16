eacd4337419c9ed38dfa41b3a504816531069104
Igor Bonadio <igorbonadio@gmail.com>
2015-01-16 10:23:52 -0200

    Test DiscreteIIDModel::evaluate

--- src/test/model/DiscreteIIDModelTest.cpp
+++ src/test/model/DiscreteIIDModelTest.cpp
@@ -8,6 +8,7 @@ using ::testing::Eq;
 using ::testing::DoubleEq;
 using tops::model::DiscreteIIDModel;
 using tops::model::DiscreteIIDModelPtr;
+using tops::model::Sequence;
 
 class ADiscreteIIDModel : public testing::Test {
 protected:
@@ -22,4 +23,19 @@ TEST_F(ADiscreteIIDModel, ShouldHaveEvaluateASingleSymbol) {
   ASSERT_THAT(model->logProbabilityOf(0), DoubleEq(log(0.5)));
   ASSERT_THAT(model->logProbabilityOf(1), DoubleEq(log(0.3)));
   ASSERT_THAT(model->logProbabilityOf(2), DoubleEq(log(0.2)));
+}
+
+TEST_F(ADiscreteIIDModel, ShouldHaveEvaluateASequence) {
+  std::vector<Sequence> test_data = {
+    {0, 0, 1, 1, 2, 2},
+    {0, 1, 2, 2, 2, 2},
+    {1, 1, 1, 0, 0, 0}
+  };
+  for (auto data : test_data) {
+    double result = 0.0;
+    for (auto symbol : data) {
+      result += model->logProbabilityOf(symbol);
+    }
+    ASSERT_THAT(model->evaluate(data, 0, 5), DoubleEq(result));
+  }
 }
