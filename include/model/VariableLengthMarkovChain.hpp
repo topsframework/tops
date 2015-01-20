10a57cf93353f418693ff2b28472505551c4b9b0
Igor Bonadio <igorbonadio@gmail.com>
2015-01-20 18:05:31 -0200

    Apply Google Style to VariableLengthMarkovChain.{cpp,hpp}

--- include/model/VariableLengthMarkovChain.hpp
+++ include/model/VariableLengthMarkovChain.hpp
@@ -1,3 +1,22 @@
+/***********************************************************************/
+/*  Copyright 2015 ToPS                                                */
+/*                                                                     */
+/*  This program is free software; you can redistribute it and/or      */
+/*  modify it under the terms of the GNU  General Public License as    */
+/*  published by the Free Software Foundation; either version 3 of     */
+/*  the License, or (at your option) any later version.                */
+/*                                                                     */
+/*  This program is distributed in the hope that it will be useful,    */
+/*  but WITHOUT ANY WARRANTY; without even the implied warranty of     */
+/*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the      */
+/*  GNU General Public License for more details.                       */
+/*                                                                     */
+/*  You should have received a copy of the GNU General Public License  */
+/*  along with this program; if not, write to the Free Software        */
+/*  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,         */
+/*  MA 02110-1301, USA.                                                */
+/***********************************************************************/
+
 #ifndef VARIABLE_LENGTH_MARKOV_CHAIN_HPP_
 #define VARIABLE_LENGTH_MARKOV_CHAIN_HPP_
 
@@ -7,21 +26,27 @@
 #include "ContextTree.hpp"
 
 namespace tops {
-  namespace model {
-    class VariableLengthMarkovChain : public FactorableModel {
-    public:
-      VariableLengthMarkovChain(ContextTreePtr tree, int alphabetSize);
+namespace model {
 
-      virtual int alphabetSize();
+class VariableLengthMarkovChain : public FactorableModel {
+ public:
+  VariableLengthMarkovChain(ContextTreePtr tree, int alphabetSize);
 
-      virtual double evaluate(const Sequence &sequence, unsigned int begin, unsigned int end);
-      virtual double evaluatePosition(const Sequence &sequence, unsigned int index) const;
+  virtual int alphabetSize();
 
-      virtual double choose() const;
-      virtual Symbol choosePosition(const Sequence &sequence, unsigned int index) const;
-    };
-    typedef std::shared_ptr<VariableLengthMarkovChain> VariableLengthMarkovChainPtr;
-  }
-}
+  virtual double evaluate(const Sequence &sequence,
+                          unsigned int begin,
+                          unsigned int end);
+  virtual double evaluatePosition(const Sequence &sequence,
+                                  unsigned int index) const;
+
+  virtual double choose() const;
+  virtual Symbol choosePosition(const Sequence &sequence,
+                                unsigned int index) const;
+};
+typedef std::shared_ptr<VariableLengthMarkovChain> VariableLengthMarkovChainPtr;
+
+}  // namespace model
+}  // namespace tops
 
 #endif
--- src/model/VariableLengthMarkovChain.cpp
+++ src/model/VariableLengthMarkovChain.cpp
@@ -1,36 +1,63 @@
+/***********************************************************************/
+/*  Copyright 2015 ToPS                                                */
+/*                                                                     */
+/*  This program is free software; you can redistribute it and/or      */
+/*  modify it under the terms of the GNU  General Public License as    */
+/*  published by the Free Software Foundation; either version 3 of     */
+/*  the License, or (at your option) any later version.                */
+/*                                                                     */
+/*  This program is distributed in the hope that it will be useful,    */
+/*  but WITHOUT ANY WARRANTY; without even the implied warranty of     */
+/*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the      */
+/*  GNU General Public License for more details.                       */
+/*                                                                     */
+/*  You should have received a copy of the GNU General Public License  */
+/*  along with this program; if not, write to the Free Software        */
+/*  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,         */
+/*  MA 02110-1301, USA.                                                */
+/***********************************************************************/
+
 #include "VariableLengthMarkovChain.hpp"
 
 #include "src/VariableLengthMarkovChain.hpp"
 
 namespace tops {
-  namespace model {
-    VariableLengthMarkovChain::VariableLengthMarkovChain(ContextTreePtr tree, int alphabetSize) {
-      _self = tops::VariableLengthMarkovChainPtr(new tops::VariableLengthMarkovChain(tree->_self));
-      auto alphabet = tops::AlphabetPtr(new Alphabet());
-      for (int i = 0; i < alphabetSize; i++) {
-        alphabet->createSymbol(std::to_string(i));
-      }
-      _self->setAlphabet(alphabet);
-    }
+namespace model {
 
-    int VariableLengthMarkovChain::alphabetSize() {
-      return _self->alphabet()->size();
-    }
-
-    double VariableLengthMarkovChain::evaluate(const Sequence &sequence, unsigned int begin, unsigned int end) {
-      return _self->evaluate(sequence, begin, end);
-    }
-
-    double VariableLengthMarkovChain::evaluatePosition(const Sequence &sequence, unsigned int index) const {
-      return _self->evaluatePosition(sequence, index);
-    }
-
-    double VariableLengthMarkovChain::choose() const {
-      return _self->choose();
-    }
-
-    Symbol VariableLengthMarkovChain::choosePosition(const Sequence &sequence, unsigned int index) const {
-      return _self->factorable()->choosePosition(sequence, index);
-    }
+VariableLengthMarkovChain::VariableLengthMarkovChain(ContextTreePtr tree,
+                                                     int alphabetSize) {
+  _self = tops::VariableLengthMarkovChainPtr(
+    new tops::VariableLengthMarkovChain(tree->_self));
+  auto alphabet = tops::AlphabetPtr(new Alphabet());
+  for (int i = 0; i < alphabetSize; i++) {
+    alphabet->createSymbol(std::to_string(i));
   }
+  _self->setAlphabet(alphabet);
 }
+
+int VariableLengthMarkovChain::alphabetSize() {
+  return _self->alphabet()->size();
+}
+
+double VariableLengthMarkovChain::evaluate(const Sequence &sequence,
+                                           unsigned int begin,
+                                           unsigned int end) {
+  return _self->evaluate(sequence, begin, end);
+}
+
+double VariableLengthMarkovChain::evaluatePosition(const Sequence &sequence,
+                                                   unsigned int index) const {
+  return _self->evaluatePosition(sequence, index);
+}
+
+double VariableLengthMarkovChain::choose() const {
+  return _self->choose();
+}
+
+Symbol VariableLengthMarkovChain::choosePosition(const Sequence &sequence,
+                                                 unsigned int index) const {
+  return _self->factorable()->choosePosition(sequence, index);
+}
+
+}  // namespace model
+}  // namespace tops
