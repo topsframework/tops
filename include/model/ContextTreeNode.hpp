f3e2b758a08c4901e4f5b1336ba5fced1085c67b
Igor Bonadio <igorbonadio@gmail.com>
2015-01-20 17:50:42 -0200

    Apply Google Style to ContextTreeNode.{cpp,hpp}

--- include/model/ContextTreeNode.hpp
+++ include/model/ContextTreeNode.hpp
@@ -1,3 +1,22 @@
+/***********************************************************************/
+/*  Copyright 2015 ToPS                                                */
+/*                                                                     */
+/*  This program is free software; you can redistribute it and/or      */
+/*  modify it under the terms of the GNU  General Public License as    */
+/*  published by the Free Software Foundation; either version 3 of     */
+/*  the License, or (at your option) any later version.                */
+/*                                                                     */
+/*  This program is distributed in the hope that it will be useful,    */
+/*  but WITHOUT ANY WARRANTY; without even the implied warranty of     */
+/*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the      */
+/*  GNU General Public License for more details.                       */
+/*                                                                     */
+/*  You should have received a copy of the GNU General Public License  */
+/*  along with this program; if not, write to the Free Software        */
+/*  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,         */
+/*  MA 02110-1301, USA.                                                */
+/***********************************************************************/
+
 #ifndef CONTEXT_TREE_NODE_HPP_
 #define CONTEXT_TREE_NODE_HPP_
 
@@ -8,24 +27,26 @@
 #include "src/ContextTree.hpp"
 
 namespace tops {
-  namespace model {
-    class ContextTreeNode;
-    typedef std::shared_ptr<ContextTreeNode> ContextTreeNodePtr;
+namespace model {
 
-    class ContextTreeNode {
-    public:
-      ContextTreeNode(DiscreteIIDModelPtr distribution);
+class ContextTreeNode;
+typedef std::shared_ptr<ContextTreeNode> ContextTreeNodePtr;
 
-      DiscreteIIDModelPtr distribution(DiscreteIIDModelPtr distribution);
-      DiscreteIIDModelPtr distribution();
-      
-      ContextTreeNodePtr child(ContextTreeNodePtr child, Symbol symbol);
-      ContextTreeNodePtr child(Symbol symbol);
+class ContextTreeNode {
+ public:
+  explicit ContextTreeNode(DiscreteIIDModelPtr iid);
 
-      // After refactoring, remove _self!
-      tops::ContextTreeNodePtr _self;
-    };
-  }
-}
+  DiscreteIIDModelPtr distribution(DiscreteIIDModelPtr iid);
+  DiscreteIIDModelPtr distribution();
+
+  ContextTreeNodePtr child(ContextTreeNodePtr child, Symbol symbol);
+  ContextTreeNodePtr child(Symbol symbol);
+
+  // After refactoring, remove _self!
+  tops::ContextTreeNodePtr _self;
+};
+
+}  // namespace model
+}  // namespace tops
 
 #endif
--- src/model/ContextTreeNode.cpp
+++ src/model/ContextTreeNode.cpp
@@ -1,32 +1,58 @@
+/***********************************************************************/
+/*  Copyright 2015 ToPS                                                */
+/*                                                                     */
+/*  This program is free software; you can redistribute it and/or      */
+/*  modify it under the terms of the GNU  General Public License as    */
+/*  published by the Free Software Foundation; either version 3 of     */
+/*  the License, or (at your option) any later version.                */
+/*                                                                     */
+/*  This program is distributed in the hope that it will be useful,    */
+/*  but WITHOUT ANY WARRANTY; without even the implied warranty of     */
+/*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the      */
+/*  GNU General Public License for more details.                       */
+/*                                                                     */
+/*  You should have received a copy of the GNU General Public License  */
+/*  along with this program; if not, write to the Free Software        */
+/*  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,         */
+/*  MA 02110-1301, USA.                                                */
+/***********************************************************************/
+
 #include "ContextTreeNode.hpp"
 
 namespace tops {
-  namespace model {
-    ContextTreeNode::ContextTreeNode(DiscreteIIDModelPtr distribution) {
-      _self = tops::ContextTreeNodePtr(new tops::ContextTreeNode(distribution->alphabetSize()));
-      this->distribution(distribution);
-    }
+namespace model {
 
-    DiscreteIIDModelPtr ContextTreeNode::distribution(DiscreteIIDModelPtr distribution) {
-      _self->setDistribution(boost::static_pointer_cast<tops::DiscreteIIDModel>(distribution->_self));
-      return distribution;
-    }
-
-    DiscreteIIDModelPtr ContextTreeNode::distribution() {
-      auto iid = DiscreteIIDModelPtr(new DiscreteIIDModel({}));
-      iid->_self = _self->getDistribution();
-      return iid;
-    }
-
-    ContextTreeNodePtr ContextTreeNode::child(ContextTreeNodePtr child, Symbol symbol) {
-      _self->setChild(child->_self, symbol);
-      return child;
-    }
-
-    ContextTreeNodePtr ContextTreeNode::child(Symbol symbol) {
-      auto child = ContextTreeNodePtr(new ContextTreeNode(DiscreteIIDModelPtr(new DiscreteIIDModel({0}))));
-      child->_self = _self->getChild(symbol);
-      return child;
-    }
-  }
+ContextTreeNode::ContextTreeNode(DiscreteIIDModelPtr iid) {
+  _self = tops::ContextTreeNodePtr(
+    new tops::ContextTreeNode(iid->alphabetSize()));
+  this->distribution(iid);
 }
+
+DiscreteIIDModelPtr ContextTreeNode::distribution(DiscreteIIDModelPtr iid) {
+  _self->setDistribution(
+    boost::static_pointer_cast<tops::DiscreteIIDModel>(iid->_self));
+  return iid;
+}
+
+DiscreteIIDModelPtr ContextTreeNode::distribution() {
+  auto iid = DiscreteIIDModelPtr(new DiscreteIIDModel({}));
+  iid->_self = _self->getDistribution();
+  return iid;
+}
+
+ContextTreeNodePtr ContextTreeNode::child(
+    ContextTreeNodePtr child,
+    Symbol symbol) {
+  _self->setChild(child->_self, symbol);
+  return child;
+}
+
+ContextTreeNodePtr ContextTreeNode::child(Symbol symbol) {
+  auto child = ContextTreeNodePtr(
+    new ContextTreeNode(DiscreteIIDModelPtr(new DiscreteIIDModel({0}))));
+  child->_self = _self->getChild(symbol);
+  return child;
+}
+
+}  // namespace model
+}  // namespace tops
