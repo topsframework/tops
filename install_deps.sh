12622683799be31573e7c0baec0db99969bdbbd4
Igor Bonadio <igorbonadio@gmail.com>
2015-01-22 12:55:57 -0200

    Add gmake to freebsd vm

--- install_deps.sh
+++ install_deps.sh
@@ -14,6 +14,7 @@ fi
 
 if [ "$1" == "freebsd" ]; then
   pkg install -y devel/git
+  pkg install -y gmake
   pkg install -y devel/boost-all
-  sudo pkg install -y devel/cmake
+  pkg install -y devel/cmake
 fi
