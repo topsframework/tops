1a772cb99fb2d284a49b0bdfe0c31bea69c4623b
Igor Bonadio <igorbonadio@gmail.com>
2015-01-28 15:33:15 -0200

    Add IMC's helper function

--- include/helper/IMC.hpp
+++ include/helper/IMC.hpp
@@ -17,35 +17,17 @@
 /*  MA 02110-1301, USA.                                                */
 /***********************************************************************/
 
-#include <vector>
-#include <random>
-#include <string>
+#ifndef TOPS_HELPER_INHOMOGENEOUS_MARKOV_CHAIN_
+#define TOPS_HELPER_INHOMOGENEOUS_MARKOV_CHAIN_
 
-#include "benchmark/benchmark.h"
-
-#include "VariableLengthMarkovChain.hpp"
-#include "Sequence.hpp"
 #include "InhomogeneousMarkovChain.hpp"
 
-#include "Random.hpp"
-#include "IMC.hpp"
+namespace tops {
+namespace helper {
 
-using tops::model::VariableLengthMarkovChain;
-using tops::model::VariableLengthMarkovChainPtr;
-using tops::model::InhomogeneousMarkovChain;
-using tops::model::InhomogeneousMarkovChainPtr;
-using tops::model::Sequence;
+tops::model::InhomogeneousMarkovChainPtr generateRandomIMC(int number_of_chains, int alphabet_size);
 
-using tops::helper::generateSequence;
-using tops::helper::generateRandomIMC;
+}  // namespace helper
+}  // namespace tops
 
-static void BM_InhomogeneousMarkovChainEvaluate(benchmark::State& state) {
-  state.PauseTiming();
-  auto model = generateRandomIMC(state.range_x(), 2);
-  auto sequence = generateSequence(state.range_y(), 2);
-  state.ResumeTiming();
-  while (state.KeepRunning()) {
-    model->evaluate(sequence, 0, state.range_y()-1);
-  }
-}
-BENCHMARK(BM_InhomogeneousMarkovChainEvaluate)->RangePair(2, 16, 2, 5*1024*1024*1024);
+#endif  // TOPS_HELPER_INHOMOGENEOUS_MARKOV_CHAIN_
---
+++ src/helper/IMC.cpp
@@ -0,0 +1,16 @@
+#include "IMC.hpp"
+
+#include "VLMC.hpp"
+
+namespace tops {
+namespace helper {
+
+tops::model::InhomogeneousMarkovChainPtr generateRandomIMC(int number_of_chains, int alphabet_size) {
+  std::vector<tops::model::VariableLengthMarkovChainPtr> vlmcs;
+  for (int i = 0; i < number_of_chains; i++)
+    vlmcs.push_back(generateRandomVLMC(number_of_chains, alphabet_size));
+  return tops::model::InhomogeneousMarkovChain::make(vlmcs, true);
+}
+
+}  // namespace helper
+}  // namespace tops
