9d5471ce4055f12895bb31392598fa510c17b1ac
Igor Bonadio <igorbonadio@gmail.com>
2015-02-02 12:01:03 -0200

    Standardize model/DiscreteIIDModel.hpp

--- include/model/DiscreteIIDModel.hpp
+++ include/model/DiscreteIIDModel.hpp
@@ -37,10 +37,14 @@ class DiscreteIIDModel {
   static DiscreteIIDModelPtr make(std::vector<double> probabilities);
 
   virtual int alphabetSize() const;
+
   virtual double probabilityOf(Symbol s) const;
   virtual double evaluatePosition(const Sequence &s, unsigned int i) const;
+  virtual double evaluateSequence(const Sequence &s,
+                                  unsigned int begin,
+                                  unsigned int end) const;
+
   virtual int choosePosition(const Sequence &s, unsigned int i) const;
-  virtual double evaluateSequence(const Sequence &s, unsigned int begin, unsigned int end) const;
 
  private:
   explicit DiscreteIIDModel(std::vector<double> probabilities);
