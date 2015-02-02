116f680ffa577fde613c1b53f8c3851f499a5494
Renato Cordeiro Ferreira <renato.cferreira@hotmail.com>
2015-02-02 15:44:54 -0200

    Add test to inhonogeneus markov chain

---
+++ test/model/InhomogeneousMarkovChainTest.cpp
@@ -0,0 +1,67 @@
+/***********************************************************************/
+/*  Copyright 2015 ToPS                                                */
+/*                                                                     */
+/*  This program is free software; you can redistribute it and/or      */
+/*  modify it under the terms of the GNU  General Public License as    */
+/*  published by the Free Software Foundation; either version 3 of     */
+/*  the License, or (at your option) any later version.                */
+/*                                                                     */
+/*  This program is distributed in the hope that it will be useful,    */
+/*  but WITHOUT ANY WARRANTY; without even the implied warranty of     */
+/*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the      */
+/*  GNU General Public License for more details.                       */
+/*                                                                     */
+/*  You should have received a copy of the GNU General Public License  */
+/*  along with this program; if not, write to the Free Software        */
+/*  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,         */
+/*  MA 02110-1301, USA.                                                */
+/***********************************************************************/
+
+// Standard headers
+#include <cmath>
+#include <vector>
+
+// External headers
+#include "gmock/gmock.h"
+
+// ToPS headers
+#include "model/InhomogeneousMarkovChain.hpp"
+#include "model/VariableLengthMarkovChain.hpp"
+#include "model/Sequence.hpp"
+
+#include "helper/VariableLengthMarkovChain.hpp"
+
+using ::testing::Eq;
+using ::testing::DoubleEq;
+
+using tops::model::Sequence;
+using tops::model::VariableLengthMarkovChain;
+using tops::model::VariableLengthMarkovChainPtr;
+using tops::model::InhomogeneousMarkovChain;
+using tops::model::InhomogeneousMarkovChainPtr;
+
+using tops::helper::createMachlerVLMC;
+using tops::helper::createVLMCMC;
+
+class AnInhomogeneousMarkovChain : public testing::Test {
+ protected:
+  InhomogeneousMarkovChainPtr imc;
+
+  virtual void SetUp() {
+    imc = InhomogeneousMarkovChain::make({createMachlerVLMC(), createVLMCMC()});
+  }
+};
+
+TEST_F(AnInhomogeneousMarkovChain, ShouldHaveAnAlphabetSize) {
+  ASSERT_THAT(imc->alphabetSize(), Eq(2));
+}
+
+TEST_F(AnInhomogeneousMarkovChain, ShouldEvaluateASequence) {
+  ASSERT_THAT(imc->evaluateSequence({0}, 0, 1), DoubleEq(log(0.50)));
+  ASSERT_THAT(imc->evaluateSequence({1}, 0, 1), DoubleEq(log(0.50)));
+  ASSERT_THAT(imc->evaluateSequence({0, 1}, 0, 2), DoubleEq(log(0.50) + log(0.90)));
+  ASSERT_THAT(imc->evaluateSequence({0, 0}, 0, 2), DoubleEq(log(0.50) + log(0.10)));
+  ASSERT_THAT(imc->evaluateSequence({1, 0}, 0, 2), DoubleEq(log(0.50) + log(0.50)));
+  ASSERT_THAT(imc->evaluateSequence({1, 1}, 0, 2), DoubleEq(log(0.50) + log(0.50)));
+  ASSERT_THAT(imc->evaluateSequence({1, 0, 1}, 0, 3), DoubleEq(-HUGE));
+}
