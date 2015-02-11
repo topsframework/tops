50342c264c36f48d79019ead9453b42c5bd077f3
Igor Bonadio <igorbonadio@gmail.com>
2015-02-11 15:40:39 -0200

    Fix layout of src/model/DiscreteIIDModel.cpp

--- src/model/DiscreteIIDModel.cpp
+++ src/model/DiscreteIIDModel.cpp
@@ -39,7 +39,8 @@ DiscreteIIDModelPtr DiscreteIIDModel::make(std::vector<double> probabilities) {
   return DiscreteIIDModelPtr(new DiscreteIIDModel(probabilities));
 }
 
-std::vector<double> DiscreteIIDModel::normalize(std::vector<double> probabilities) {
+std::vector<double> DiscreteIIDModel::normalize(
+    std::vector<double> probabilities) {
   double sum = 0;
   for (auto p : probabilities)
     sum += p;
@@ -128,7 +129,6 @@ DiscreteIIDModelPtr DiscreteIIDModel::trainSmoothedHistogramStanke(
       unsigned int max_length,
       int m,
       double slope) {
-
   double a = slope;
   unsigned int max = max_length;
   unsigned int L = max;
@@ -148,8 +148,8 @@ DiscreteIIDModelPtr DiscreteIIDModel::trainSmoothedHistogramStanke(
   pi.resize(L);
 
   if (data.size() > 0) {
-    for(unsigned int i = 0; i < data.size(); i++){
-      if(counter.find(data[i]) == counter.end())
+    for (unsigned int i = 0; i < data.size(); i++) {
+      if (counter.find(data[i]) == counter.end())
         counter[data[i]] = 1.0;
       else
         counter[data[i]] += 1.0;
@@ -159,36 +159,38 @@ DiscreteIIDModelPtr DiscreteIIDModel::trainSmoothedHistogramStanke(
     double count_right = 0;
 
     for (int pos = 0; (pos < L) && (pos < max) ; pos +=1) {
-      int bwd = (int) (.01 + (a / pow(L, 1.0/5.0) ) * (double) pos);
-      if(bwd <= 0)
+      int bwd = static_cast<int>(
+        .01 + (a / pow(L, 1.0/5.0) ) * static_cast<double>(pos));
+      if (bwd <= 0)
         bwd = 1;
-      for(int j = pos - bwd + 1;  (j <= pos + bwd -1)  ; j++) {
-        if (! (j >= 0 && j < L))
+      for (int j = pos - bwd + 1;  (j <= pos + bwd -1)  ; j++) {
+        if (!(j >= 0 && j < L))
           continue;
-        if(j <= pos)
+        if (j <= pos)
           count_left += (counter[j]) ? 1: 0;
-        if(j >= pos)
+        if (j >= pos)
           count_right += (counter[j])? 1: 0;
       }
 
       while (count_left < m && count_right < m && bwd < L) {
-        bwd ++;
-        if(pos + bwd -1 < L)
+        bwd++;
+        if (pos + bwd -1 < L)
           count_left += counter[pos + bwd - 1] ? 1:0;
-        if(pos - bwd + 1 >= 0)
+        if (pos - bwd + 1 >= 0)
           count_right += counter[pos + bwd - 1] ? 1:0;
       }
 
-      if(pos < L)
-        pi[pos] += kernel_normal((double)0, (double)bwd) * counter[pos];
+      if (pos < L)
+        pi[pos] += kernel_normal(static_cast<double>(0),
+                                 static_cast<double>(bwd)) * counter[pos];
       bool negligible = false;
-      int j=1;
-      while (!negligible && (pos-j>=0 || pos+j<L)){
-        double  wj = kernel_normal(j, bwd) * (counter[pos] );
-        if (pos-j>=0 && pos-j<(int)pi.size() ) {
+      int j = 1;
+      while (!negligible && (pos-j >= 0 || pos+j < L)) {
+        double  wj = kernel_normal(j, bwd) * counter[pos];
+        if (pos-j >= 0 && pos-j < static_cast<int>(pi.size())) {
           pi[pos-j] += wj;
         }
-        if (pos+j<(int)pi.size() && pos+j>=0) {
+        if (pos+j < static_cast<int>(pi.size()) && pos+j >= 0) {
           pi[pos+j] += wj;
         }
         negligible = (wj < 1e-20);
@@ -201,7 +203,7 @@ DiscreteIIDModelPtr DiscreteIIDModel::trainSmoothedHistogramStanke(
       total += pi[k];
     prob.resize(L);
     for (long k = 0; k < (int)pi.size(); k++) {
-      prob[k] =  pi[k]/(total) ;
+      prob[k] = pi[k]/(total) ;
     }
   }
 
@@ -220,61 +222,63 @@ DiscreteIIDModelPtr DiscreteIIDModel::trainSmoothedHistogramKernelDensity(
     }
   }
 
-  std::map<long,double> sum;
+  std::map<long, double> sum;
   double total = 0.0;
 
-  if(data.size() > 0)
-    {
-      double bandwidth = sj_bandwidth(data);
+  if (data.size() > 0) {
+    double bandwidth = sj_bandwidth(data);
 
-      for (int pos = 0; pos <= max; pos++) {
-        sum[pos] = 0.0;
-        double integral = 0.0;
-        double min = kernel_density_estimation(pos-0.5, bandwidth, data);
-        double max2 = kernel_density_estimation(pos+0.5, bandwidth, data);
-        if(max2 < min) {
-          double aux = min;
-          min = max2;
-          max2 = aux;
-        }
-        integral += min + (max2 - min)/2;
-        sum[pos] = integral;
-        total += integral;
+    for (int pos = 0; pos <= max; pos++) {
+      sum[pos] = 0.0;
+      double integral = 0.0;
+      double min = kernel_density_estimation(pos-0.5, bandwidth, data);
+      double max2 = kernel_density_estimation(pos+0.5, bandwidth, data);
+      if (max2 < min) {
+        double aux = min;
+        min = max2;
+        max2 = aux;
       }
+      integral += min + (max2 - min)/2;
+      sum[pos] = integral;
+      total += integral;
     }
+  }
 
   std::vector<double> prob;
   prob.resize(max+2);
-  for (int k = 0; k <= max; k++){
-    prob[k] =  sum[k]/total;
+  for (int k = 0; k <= max; k++) {
+    prob[k] = sum[k]/total;
   }
 
   return DiscreteIIDModel::make(normalize(prob));
 }
 
 double DiscreteIIDModel::kernel_normal(double x, double h) {
-  double y = (x/h) * (x/h) ;
+  double y = (x/h) * (x/h);
   double f = 1.0/(sqrt(2*M_PI));
-  double v =  (f/h) * exp (- y/2);
+  double v = (f/h) * exp(-y/2);
   return v;
 }
 
 /* Epanechnikov kernel */
-  double DiscreteIIDModel::epanechnikov(double x, double h){
-    double a = h * sqrt(5.0);
-    double absx = fabs(x);
-    if(absx < a ) {
-      return (3.0/4.0) * (1.0 - (absx/a)*(absx/a))/a;
-    } else {
-      return 0.0;
-    }
+double DiscreteIIDModel::epanechnikov(double x, double h) {
+  double a = h * sqrt(5.0);
+  double absx = fabs(x);
+  if (absx < a ) {
+    return (3.0/4.0) * (1.0 - (absx/a)*(absx/a))/a;
+  } else {
+    return 0.0;
   }
+}
 
 
 // code from R-1.7.0/src/appl/bandwidths.c
 #define abs9(a) (a > 0 ? a:-a)
-  void DiscreteIIDModel::band_den_bin(int n, int nb, double *d, const std::vector<double> &x,  std::vector<double> &cnt)
-  {
+  void DiscreteIIDModel::band_den_bin(int n,
+                                      int nb,
+                                      double *d,
+                                      const std::vector<double> &x,
+                                      std::vector<double> &cnt) {
     int   i, j,  nn = n;
     int ii, jj, iij;
     double xmin, xmax, rang, dd;
@@ -296,105 +300,128 @@ double DiscreteIIDModel::kernel_normal(double x, double h) {
       }
     }
   }
-  void DiscreteIIDModel::band_phi6_bin(int n, int nb, double d, std::vector<double> &x, double h, double *u)
-  {
-    int   i, nn = n, nbin = nb;
-    double delta, sum, term;
-    sum = 0.0;
-    for (i = 0; i < nbin; i++) {
-      delta = i * (d) / (h);
-      delta *= delta;
-      if (delta >= 1000) break;
-      term = exp(-delta / 2) *
-        (delta * delta * delta - 15 * delta * delta + 45 * delta - 15);
-      sum += term * x[i];
-    }
-    sum = 2 * sum - 15 * nn;    /* add in diagonal */
-    *u = sum / (nn * (nn - 1) * pow(h, 7.0) * sqrt(2 * M_PI));
+  void DiscreteIIDModel::band_phi6_bin(int n,
+                                       int nb,
+                                       double d,
+                                       std::vector<double> &x,
+                                       double h,
+                                       double *u) {
+  int   i, nn = n, nbin = nb;
+  double delta, sum, term;
+  sum = 0.0;
+  for (i = 0; i < nbin; i++) {
+    delta = i * (d) / (h);
+    delta *= delta;
+    if (delta >= 1000) break;
+    term = exp(-delta / 2) *
+      (delta * delta * delta - 15 * delta * delta + 45 * delta - 15);
+    sum += term * x[i];
   }
-  void
-  DiscreteIIDModel::band_phi4_bin(int n, int nb, double d, std::vector<double> x, double h, double *u)
-  {
-    int   i, nn = n, nbin = nb;
-    double delta, sum, term;
+  sum = 2 * sum - 15 * nn;    /* add in diagonal */
+  *u = sum / (nn * (nn - 1) * pow(h, 7.0) * sqrt(2 * M_PI));
+}
+void DiscreteIIDModel::band_phi4_bin(int n,
+                                     int nb,
+                                     double d,
+                                     std::vector<double> x,
+                                     double h,
+                                     double *u) {
+  int   i, nn = n, nbin = nb;
+  double delta, sum, term;
 
-    sum = 0.0;
-    for (i = 0; i < nbin; i++) {
-      delta = i * (d) / (h);
-      delta *= delta;
-      if (delta >= 1000) break;
-      term = exp(-delta / 2) * (delta * delta - 6 * delta + 3);
-      sum += term * x[i];
-    }
-    sum = 2 * sum + nn * 3;     /* add in diagonal */
-    *u = sum / (nn * (nn - 1) * pow(h, 5.0) * sqrt(2 * M_PI));
+  sum = 0.0;
+  for (i = 0; i < nbin; i++) {
+    delta = i * (d) / (h);
+    delta *= delta;
+    if (delta >= 1000) break;
+    term = exp(-delta / 2) * (delta * delta - 6 * delta + 3);
+    sum += term * x[i];
   }
+  sum = 2 * sum + nn * 3;     /* add in diagonal */
+  *u = sum / (nn * (nn - 1) * pow(h, 5.0) * sqrt(2 * M_PI));
+}
 
 
-  double DiscreteIIDModel::mean(const std::vector<double> &data){
-    double sum = 0.0;
-    for(unsigned int i = 0; i < data.size(); i++){
-      sum += data[i];
-    }
-    return sum/(double)data.size();
+double DiscreteIIDModel::mean(const std::vector<double> &data) {
+  double sum = 0.0;
+  for (unsigned int i = 0; i < data.size(); i++) {
+    sum += data[i];
   }
+  return sum/static_cast<double>(data.size());
+}
 
-  double DiscreteIIDModel::var(const std::vector<double> &data){
-    double data_mean = mean(data);
-    double sum = 0.0;
-    for(unsigned int i = 0; i < data.size(); i++){
-      sum += (data[i] - data_mean)*(data[i] - data_mean);
-    }
-    return sum/( (double) data.size() -1.0);
+double DiscreteIIDModel::var(const std::vector<double> &data) {
+  double data_mean = mean(data);
+  double sum = 0.0;
+  for (unsigned int i = 0; i < data.size(); i++) {
+    sum += (data[i] - data_mean)*(data[i] - data_mean);
   }
+  return sum/(static_cast<double>(data.size()) -1.0);
+}
 
-  /* quantile */
-  double DiscreteIIDModel::quantile (std::vector<double> data, double q){
-    int low_index = (int)floor(q * ((double)data.size()-1));
-    int high_index = (int)ceil(q * ((double)data.size()-1));
-    double h =  q * ((double)data.size() -1) - (double)low_index;
-    sort(data.begin(), data.end());
-    return (1-h)*data[low_index] + h * data[high_index];
+/* quantile */
+double DiscreteIIDModel::quantile(std::vector<double> data, double q) {
+  int low_index = static_cast<int>(
+    floor(q * (static_cast<double>(data.size()-1))));
+  int high_index = static_cast<int>(
+    ceil(q * (static_cast<double>(data.size()-1))));
+  double h =  q * (static_cast<double>(
+    data.size() -1)) - static_cast<double>(low_index);
+  sort(data.begin(), data.end());
+  return (1-h)*data[low_index] + h * data[high_index];
+}
+
+/* interquantile */
+double DiscreteIIDModel::iqr(const std::vector<double> &data) {
+  double q1 = quantile(data, 0.25);
+  double q2 = quantile(data, 0.75);
+  return q2 - q1;
+}
+
+double DiscreteIIDModel::kernel_density_estimation(
+    double x,
+    double bw,
+    const std::vector<double> &data) {
+  double result = 0.0;
+  for (unsigned int i = 0; i < data.size(); i++) {
+    double x_xi = static_cast<double>(x) - static_cast<double>(data[i]);
+    result += epanechnikov(x_xi, bw);
   }
+  result /= data.size();
+  return result;
+}
 
-  /* interquantile */
-  double DiscreteIIDModel::iqr (const std::vector<double> &data){
-    double q1=  quantile(data, 0.25);
-    double q2 = quantile(data, 0.75);
-    return q2 - q1;
-  }
-
-double DiscreteIIDModel::kernel_density_estimation(double x, double bw, const std::vector<double> &data){
-    double result = 0.0;
-    for(unsigned int i = 0; i < data.size(); i++) {
-      double x_xi = (double)(x) - (double)data[i];
-      result += epanechnikov(x_xi, bw);
-    }
-    result /= data.size();
-    return result;
-  }
-
-  double DiscreteIIDModel::sj_bandwidth(const std::vector<double> &data){
-    double n = (double) data.size();
-    int nb = 1000;
-    double d = 1.0;
-    double variance =  var(data);
+double DiscreteIIDModel::sj_bandwidth(const std::vector<double> &data) {
+  double n = static_cast<double>(data.size());
+  int nb = 1000;
+  double d = 1.0;
+  double variance =  var(data);
   std::vector<double> count;
-  band_den_bin ((int)n, nb, &d, data, count);
+  band_den_bin(static_cast<int>(n), nb, &d, data, count);
   double scale = std::min(std::sqrt(variance), iqr(data)/1.349);
   double b = 1.23 * scale * pow(n, (-1.0/9.0));
   double c1 = 1.0 / (2.0*sqrt(M_PI) * n);
   double td;
-  band_phi6_bin((int)n, (int)count.size(), d, count, b, &td);
+  band_phi6_bin(static_cast<int>(n),
+                static_cast<int>(count.size()),
+                d,
+                count,
+                b,
+                &td);
   td = -td;
-  if(td < 0 || td != td){
+  if (td < 0 || td != td) {
     //    cerr << "sj_bandwidth (WARNING). Dataset very sparse !!!\n" << endl;
     return 0.001;
   }
-  double sdh ;
-  band_phi4_bin((int)n, (int)count.size(), d, count, pow(2.394/(n*td), (1.0/7.0)), &sdh);
+  double sdh;
+  band_phi4_bin(static_cast<int>(n),
+                static_cast<int>(count.size()),
+                d,
+                count,
+                pow(2.394/(n*td), (1.0/7.0)),
+                &sdh);
   return pow((c1/sdh), 1.0/5.0);
-  }
+}
 
 int DiscreteIIDModel::alphabetSize() const {
   return _probabilities.size();
