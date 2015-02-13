020d0d33a6046d870d19edf504de972b59dd08da
Igor Bonadio <igorbonadio@gmail.com>
2015-02-13 08:22:24 -0200

    [skip ci] Make script/doc.sh executable

--- script/doc.sh
+++ script/doc.sh
@@ -7,6 +7,6 @@ git checkout gh-pages
 rm -rf doc/api
 cp -R doc/doxygen/html doc/api
 git add doc/api
-git commit -m "Add documentation of $last_commit"
+git commit -m "[skip ci] Add documentation of $last_commit"
 git push origin gh-pages
 git checkout $current_banch
