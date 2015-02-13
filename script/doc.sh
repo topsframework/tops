1f7bc3a857f73e5d8642d295bed269ed1e05342a
Igor Bonadio <igorbonadio@gmail.com>
2015-02-13 08:04:32 -0200

    [skip ci] Add doc.sh, a script to generate doc/api website

---
+++ script/doc.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+current_banch=$(git symbolic-ref --short -q HEAD)
+last_commit=$(git rev-parse HEAD)
+
+git checkout gh-pages
+rm -rf doc/api
+cp -R doc/doxygen/html doc/api
+git add doc/api
+git commit -m "Add documentation of $last_commit"
+git push origin gh-pages
+git checkout $current_banch
