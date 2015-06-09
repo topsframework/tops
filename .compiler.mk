373342a6d515db5f4aa5356694c724351788b315
Renato Cordeiro Ferreira <renato.cferreira@hotmail.com>
2015-06-09 08:58:55 -0300

    Add configuration file to overwrite default compiler from environment variables

---
+++ .compiler.mk
@@ -0,0 +1,11 @@
+
+########################################################################
+##                    ToPS Compiler Configuration                     ##
+########################################################################
+
+AR     := $(or $(shell echo $$AR),$(AR))
+AS     := $(or $(shell echo $$AS),$(AS))
+CC     := $(or $(shell echo $$CC),$(CC))
+FC     := $(or $(shell echo $$FC),$(FC))
+CXX    := $(or $(shell echo $$CXX),$(CXX))
+RANLIB := $(or $(shell echo $$RANLIB),$(RANLIB))
