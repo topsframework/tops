d9abb6cf94914749232a79a3bbc7058b99faf9fe
Igor Bonadio <igorbonadio@gmail.com>
2016-01-27 14:28:52 -0200

    Remove old benchmarks

--- benchmark/bench.cpp
+++ benchmark/bench.cpp
@@ -19,14 +19,4 @@
 
 #include "benchmark/benchmark.h"
 
-// Augment the main() program to invoke benchmarks if specified
-// via the --benchmarks command line flag.  E.g.,
-//       my_unittest --benchmark_filter=all
-//       my_unittest --benchmark_filter=BM_StringCreation
-//       my_unittest --benchmark_filter=String
-//       my_unittest --benchmark_filter='Copy|Creation'
-int main(int argc, const char* argv[]) {
-  benchmark::Initialize(&argc, argv);
-  benchmark::RunSpecifiedBenchmarks();
-  return 0;
-}
+BENCHMARK_MAIN();
