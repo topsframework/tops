20c3a4fa4318435f83c9057683a4f210a8952ffc
Renato Cordeiro Ferreira <renato.cferreira@hotmail.com>
2016-02-03 10:55:25 -0200

    Add warning '-Wmissing-include-dirs'

--- Config.mk
+++ Config.mk
@@ -47,7 +47,7 @@ LDLIBS          +=
 CPPFLAGS        += # Precompiler Flags
 ASFLAGS         += # Assembly Flags
 CFLAGS          += # C Flags
-CXXFLAGS        += -std=c++14 -Wall -Wextra -Wshadow -Wcast-align -Werror -Wold-style-cast -pedantic -O2
+CXXFLAGS        += -std=c++14 -Wall -Wextra -Wshadow -Wcast-align -Werror -Wold-style-cast -Wmissing-include-dirs -pedantic -O2
 LDFLAGS         +=
 
 # Makeball list
