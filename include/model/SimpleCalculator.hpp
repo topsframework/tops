3ad1d4014fca0e33c82af4328f21479ae2b66d25
Igor Bonadio <igorbonadio@gmail.com>
2016-10-15 11:44:21 -0300

    Fix constructor of 'SimpleCalculator'

--- include/model/SimpleCalculator.hpp
+++ include/model/SimpleCalculator.hpp
@@ -98,7 +98,7 @@ class SimpleCalculator : public Calculator {
                    std::vector<Sequence> other_sequences)
       : _model(std::move(model)),
         _sequence(std::move(sequence)),
-        _other_sequences(other_sequences) {
+        _other_sequences(std::move(other_sequences)) {
   }
 
  private:
