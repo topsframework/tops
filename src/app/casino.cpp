fe4682aef65a4e68a8d3d4201f31f888512b922c
Bruno Tenório <bruno.tenorio1@gmail.com>
2017-11-07 23:06:22 -0200

    Fix GHMM's graph connection
    Create a function that fills a vector with probabilities from a vector
    with indices.

--- src/app/casino.cpp
+++ src/app/casino.cpp
@@ -691,6 +691,23 @@ void generate_chart(ProbabilisticModelPtr model, std::string name,
   f << x.str() << y.str();
 }
 
+std::vector<Probability> *cria_vetor_transicao(std::vector<unsigned int> vetor_indices,
+  std::vector<Probability> &vetor_probabilidades){
+
+   std::vector<Probability> *retorno = new vector<Probability>;
+
+   for(int i = 0; i < 56; i++){
+     retorno->push_back(0);
+   }
+
+   for(unsigned int i = 0; i < vetor_indices.size(); i++){
+     retorno->insert(retorno->begin() + vetor_indices.at(i),
+     vetor_probabilidades.at(i));
+   }
+
+   return retorno;
+}
+
 int main() {
 	std::cerr << "|------------------------------------|" << std::endl;
 	std::cerr << "|                MYOP                |" << std::endl;
@@ -802,84 +819,104 @@ int main() {
   DiscreteIIDModelPtr fair_geometric_transition
       = DiscreteIIDModel::make(std::vector<Probability>{{ 0.9, 0.1}});
 
-    DiscreteIIDModelPtr fair_emission
-      = DiscreteIIDModel::make(std::vector<Probability>{{
-        0.166666666, 0.166666666, 0.166666666,
-         0.166666666, 0.166666666, 0.166666666}});
-
-    DiscreteIIDModelPtr loaded_geometric_transition
-      = DiscreteIIDModel::make(std::vector<Probability>{{ 0.1, 0.9}});
-
-    DiscreteIIDModelPtr loaded_emission
+  DiscreteIIDModelPtr fair_emission
     = DiscreteIIDModel::make(std::vector<Probability>{{
-       0.5, 0.1, 0.1,
-        0.1, 0.1, 0.1}});
+      0.166666666, 0.166666666, 0.166666666,
+       0.166666666, 0.166666666, 0.166666666}});
+
+  DiscreteIIDModelPtr loaded_geometric_transition
+    = DiscreteIIDModel::make(std::vector<Probability>{{ 0.1, 0.9}});
+
+  DiscreteIIDModelPtr loaded_emission
+  = DiscreteIIDModel::make(std::vector<Probability>{{
+     0.5, 0.1, 0.1,
+      0.1, 0.1, 0.1}});
 
 
-    GHMM::StatePtr fair_state //um modelo qq
-      = GHMM::State::make(
-        0, fair_emission, //prob de emissão
-        fair_geometric_transition, //prob de trans
-        GeometricDuration::make(0, fair_geometric_transition)); //prob de duração geo
+  GHMM::StatePtr fair_state //um modelo qq
+    = GHMM::State::make(
+      0, fair_emission, //prob de emissão
+      fair_geometric_transition, //prob de trans
+      GeometricDuration::make(0, fair_geometric_transition)); //prob de duração geo
 
-    GHMM::StatePtr loaded_state //um modelo qq
-      = GHMM::State::make(
-        1, loaded_emission, //prob de emissão
-        loaded_geometric_transition, //prob de trans
-        GeometricDuration::make(1
-          , loaded_geometric_transition)); //prob de duração geo
+  GHMM::StatePtr loaded_state //um modelo qq
+    = GHMM::State::make(
+      1, loaded_emission, //prob de emissão
+      loaded_geometric_transition, //prob de trans
+      GeometricDuration::make(1
+        , loaded_geometric_transition)); //prob de duração geo
 
-    //junta tudo
-    GeneralizedHiddenMarkovModelPtr ghmm
-      = GeneralizedHiddenMarkovModel::make(
-        std::vector<GeneralizedHiddenMarkovModel::StatePtr>{
-          fair_state, //estado 0
-          loaded_state }, // estado 1
-        DiscreteIIDModel::make(std::vector<Probability>{{ 0.5, 0.5}}), //dist de estados ini
-        2, //numero de estados
-         6); //cardinalidade do alfabeto de observacoes
+  //junta tudo
+  GeneralizedHiddenMarkovModelPtr ghmm
+    = GeneralizedHiddenMarkovModel::make(
+      std::vector<GeneralizedHiddenMarkovModel::StatePtr>{
+        fair_state, //estado 0
+        loaded_state }, // estado 1
+      DiscreteIIDModel::make(std::vector<Probability>{{ 0.5, 0.5}}), //dist de estados ini
+      2, //numero de estados
+       6); //cardinalidade do alfabeto de observacoes
 
-         loaded_state->addSuccessor(0);
-         loaded_state->addSuccessor(1);
-         loaded_state->addPredecessor(0);
-         loaded_state->addPredecessor(1);
+  loaded_state->addSuccessor(0);
+  loaded_state->addSuccessor(1);
+  loaded_state->addPredecessor(0);
+  loaded_state->addPredecessor(1);
 
-         fair_state->addSuccessor(0);
-         fair_state->addSuccessor(1);
-         fair_state->addPredecessor(0);
-         fair_state->addPredecessor(1);
+  fair_state->addSuccessor(0);
+  fair_state->addSuccessor(1);
+  fair_state->addPredecessor(0);
+  fair_state->addPredecessor(1);
 
-         Sequence observation {
-                   3, 0, 0, 0, 4, 1, 1, 0, 1, 4, 3, 4, 5, 3, 1, 5, 1, 3, 5, 0,
-                    3, 0, 0, 0, 0, 3, 0, 0, 0, 0, 3, 3, 5, 2, 0, 1, 3, 0, 1,
-                     2, 4, 1, 4, 1, 1, 0, 0, 5, 1, 5, 2, 2, 1, 0, 1, 3, 1, 1,
-                      0, 2, 1, 0, 2, 2, 3, 5, 2, 0, 0, 3, 0, 1, 0, 0, 0, 2, 0,
-                       5, 3, 0, 3, 1, 2, 1, 5, 5, 2, 2, 2, 0, 2, 0, 1, 0, 3, 2,
-                        2, 4, 2, 3};
+  Sequence observation {3, 0, 0, 0, 4, 1, 1, 0, 1, 4, 3, 4, 5, 3, 1, 5, 1, 3,
+     5, 0, 3, 0, 0, 0, 0, 3, 0, 0, 0, 0, 3, 3, 5, 2, 0, 1, 3, 0, 1,
+     2, 4, 1, 4, 1, 1, 0, 0, 5, 1, 5, 2, 2, 1, 0, 1, 3, 1, 1, 0, 2, 1, 0, 2,
+     2, 3, 5, 2, 0, 0, 3, 0, 1, 0, 0, 0, 2, 0, 5, 3, 0, 3, 1, 2, 1, 5, 5, 2,
+     2, 2, 0, 2, 0, 1, 0, 3, 2, 2, 4, 2, 3};
 
 
-        auto labeler = ghmm->labeler(observation, true);
-        auto estimation = labeler->labeling(Labeler::method::bestPath);
-        auto labeling = estimation.estimated();
+  auto labeler = ghmm->labeler(observation, true);
+  auto estimation = labeler->labeling(Labeler::method::bestPath);
+  auto labeling = estimation.estimated();
 
-        for (auto symbol : labeling.label()) {
-          std::cout << symbol;
-        }
-        std::cout << std::endl << "-----" << std::endl;
+  for (auto symbol : labeling.label()) {
+    std::cout << symbol;
+  }
+  std::cout << std::endl << "-----" << std::endl;
 
-        observation =  {1, 3, 4, 3, 3, 0, 4, 2, 5, 4, 0, 3, 1, 0, 3, 2, 2, 1, 3, 2,
-           0, 4, 4, 5, 1, 4, 5, 0, 4, 3, 1, 0, 5, 0, 0, 3, 0, 0, 4, 3, 4, 1, 4,
-            0, 2, 5, 0, 3, 0, 1, 0, 4, 4, 0, 5, 2, 0, 3, 4, 4, 1, 2, 4, 1, 5,
-             5, 0, 4, 1, 3, 5, 2, 3, 1, 2, 4, 3, 5, 1, 3, 5, 1, 0, 0, 5, 0, 0,
-              0, 5, 4, 0, 0, 2, 0, 0, 4, 1, 0, 0, 0};
-        labeler = ghmm->labeler(observation, true);
-        estimation = labeler->labeling(Labeler::method::bestPath);
-        labeling = estimation.estimated();
+  observation =  {1, 3, 4, 3, 3, 0, 4, 2, 5, 4, 0, 3, 1, 0, 3, 2, 2, 1, 3, 2,
+    0, 4, 4, 5, 1, 4, 5, 0, 4, 3, 1, 0, 5, 0, 0, 3, 0, 0, 4, 3, 4, 1, 4, 0, 2,
+    5, 0, 3, 0, 1, 0, 4, 4, 0, 5, 2, 0, 3, 4, 4, 1, 2, 4, 1, 5, 5, 0, 4, 1, 3,
+    5, 2, 3, 1, 2, 4, 3, 5, 1, 3, 5, 1, 0, 0, 5, 0, 0, 0, 5, 4, 0, 0, 2, 0, 0,
+    4, 1, 0, 0, 0};
 
-        for (auto symbol : labeling.label()) {
-          std::cout << symbol;
-        }
-        std::cout << std::endl;
-        
-	return 0;
+  labeler = ghmm->labeler(observation, true);
+  estimation = labeler->labeling(Labeler::method::bestPath);
+  labeling = estimation.estimated();
+
+  for (auto symbol : labeling.label()) {
+    std::cout << symbol;
+  }
+  std::cout << std::endl;
+
+  std::vector<Probability> vetor_probabilidades;
+  vetor_probabilidades = {1, 0.5, 0.8, 0.6};
+  std::vector<unsigned int> vetor_indices;
+  vetor_indices.push_back(0);
+  vetor_indices.push_back(3);
+  vetor_indices.push_back(5);
+  vetor_indices.push_back(55);
+
+
+  std::vector<Probability> *a = cria_vetor_transicao(vetor_indices,
+  vetor_probabilidades);
+
+  cout << a->size() << endl;
+  cout << vetor_indices.size() << endl;
+  cout << vetor_probabilidades.size() << endl;
+
+  for(unsigned int i = 0; i < a->size(); i++){
+    cout << a->at(i) << " ";
+  }
+  std::cout << std::endl;
+
+  return 0;
 }
